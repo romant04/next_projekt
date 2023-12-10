cacde85b2962500bcaf19bb2d3d88b6365885a49
romant04 <roman.tarnai.04@gmail.com>
2023-12-10 20:36:44 +0100

    fix: corrected import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,26 @@
+"use client";
+
 import Link from "next/link";
-import { Todo as TodoType } from ".prisma/client";
 import { Todo } from "@/components/todo";
+import { Todo as TodoType } from "@prisma/client";
+import { useEffect, useState } from "react";
 
-async function getTodos() {
-  const res = await fetch(
-    `${
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://next-projekt.vercel.app"
-    }/api/todo`,
-    {
-      method: "GET",
-    }
-  );
-  const data = await res.json();
-  return await data.data;
-}
-
-export default async function Home() {
-  const todos: TodoType[] = await getTodos();
+export default function Home() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/todo", {
+        method: "GET",
+      });
+      const data = await res.json();
+      setTodos(data.data);
+    };
+    void fetchData();
+  }, []);
 
   return (
     <div className="text-white">
-      <div className="flex justify-between items-center mb-5">
+      <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl">My todo&apos;s</h1>
         <Link
           className="bg-yellow-300 rounded-md px-4 py-2 text-background hover:bg-yellow-200"
